a5ad28b17ba281e161cad1599ef2ce159491c677
Rohit Giri <rohit.g@protectt.ai>
2021-07-30 20:16:22 +0530

    created slider with card

---
+++ app/src/main/java/com/example/viewpagerscrollview/ViewPagerAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.viewpagerscrollview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_view_pager.view.*
+
+class ViewPagerAdapter(private val quotes : List<String>) : RecyclerView.Adapter<ViewPagerAdapter.ViewPagerViewHolder>() {
+    inner class ViewPagerViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view_pager,parent,false)
+        return ViewPagerViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
+        val curQuote = quotes[position%quotes.size]
+        holder.itemView.ivText.text = curQuote
+    }
+
+    override fun getItemCount(): Int {
+        return Int.MAX_VALUE
+    }
+}
